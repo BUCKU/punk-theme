d3394522b293af5205f6efeb08cbb31fbbfe8cf0
GitHub Action <action@github.com>
2022-08-13 18:28:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/bucku/punktheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bucku/punktheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bucku.punktheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bucku.punktheme.MyBundle
 
 class MyApplicationService {
 
